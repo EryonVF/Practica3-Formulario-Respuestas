b85be695e5bc382f8c6665da915f070d78332e59
EryonVF <146021991+EryonVF@users.noreply.github.com>
2023-10-05 17:57:23 -0700

    Nuevo diseño

--- Práctica 3 Formularios/scriptResultados.js
+++ Práctica 3 Formularios/scriptResultados.js
@@ -1,19 +1,11 @@
-// scriptResultados.js (en la página "resultados.html")
 document.addEventListener("DOMContentLoaded", function() {
     const tablaResultados = document.getElementById("tablaResultados");
 
     // Obtener los envíos almacenados en el almacenamiento local
     let envios = JSON.parse(localStorage.getItem("envios")) || [];
 
-    // Función para eliminar los datos de la tabla y del almacenamiento local
-    function eliminarDatos() {
-        tablaResultados.innerHTML = ""; // Limpia el contenido de la tabla
-        envios = []; // Borra todos los datos en el arreglo envios
-        localStorage.removeItem("envios"); // Borra los datos almacenados en el almacenamiento local
-    }
-
     // Recorrer los envíos y agregar filas a la tabla
-    envios.forEach((envio) => {
+    envios.forEach((envio, index) => {
         const fila = document.createElement("tr");
         fila.innerHTML = `
             <td>${envio.id}</td>
@@ -23,10 +15,49 @@ document.addEventListener("DOMContentLoaded", function() {
             <td>${envio.correo}</td>
             <td>${envio.edad}</td>
             <td>${envio.fechaNacimiento}</td>
+            <td><button class="btn btn-danger eliminar-btn" data-index="${index}">Eliminar</button></td>
         `;
         tablaResultados.appendChild(fila);
     });
 
-    // Agregar un evento de clic al botón "Eliminar Datos" para eliminar permanentemente los datos
-    document.getElementById("eliminarDatos").addEventListener("click", eliminarDatos);
+    // Agregar un evento de clic al botón "Eliminar Todos" para eliminar todos los datos
+    document.getElementById("eliminarDatos").addEventListener("click", function() {
+        envios = []; // Borra todos los datos en el arreglo envios
+        actualizarTablaYLocalStorage();
+    });
+
+    // Agregar un evento de clic a los botones de eliminación
+    const botonesEliminar = document.querySelectorAll(".eliminar-btn");
+    botonesEliminar.forEach((boton) => {
+        boton.addEventListener("click", function() {
+            const index = parseInt(this.getAttribute("data-index"));
+            envios.splice(index, 1);// Eliminar el registro del arreglo envios
+            actualizarTablaYLocalStorage();// Actualizar la tabla y el almacenamiento local
+        });
+    });
+
+    // Función para actualizar la tabla y el almacenamiento local
+    function actualizarTablaYLocalStorage() {
+        // Limpiar la tabla
+        tablaResultados.innerHTML = "";
+
+        // Recrear las filas con los registros actualizados
+        envios.forEach((envio, index) => {
+            const fila = document.createElement("tr");
+            fila.innerHTML = `
+                <td>${envio.id}</td>
+                <td>${envio.nombre}</td>
+                <td>${envio.apellidos}</td>
+                <td>${envio.telefono}</td>
+                <td>${envio.correo}</td>
+                <td>${envio.edad}</td>
+                <td>${envio.fechaNacimiento}</td>
+                <td><button class="btn btn-danger eliminar-btn" data-index="${index}">Eliminar</button></td>
+            `;
+            tablaResultados.appendChild(fila);
+        });
+
+        // Actualizar el almacenamiento local con los registros actualizados
+        localStorage.setItem("envios", JSON.stringify(envios));
+    }
 });
