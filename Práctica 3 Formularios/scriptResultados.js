6dceb203b6be6a7d374a4fb998c47f7cc4040b43
EryonVF <146021991+EryonVF@users.noreply.github.com>
2023-10-05 17:32:28 -0700

    Add files via upload

--- Práctica 3 Formularios/scriptResultados.js
+++ Práctica 3 Formularios/scriptResultados.js
@@ -1,9 +1,18 @@
 // scriptResultados.js (en la página "resultados.html")
 document.addEventListener("DOMContentLoaded", function() {
     const tablaResultados = document.getElementById("tablaResultados");
-      // Obtener los envíos almacenados en el almacenamiento local
-    const envios = JSON.parse(localStorage.getItem("envios")) || [];
-     // Recorrer los envíos y agregar filas a la tabla
+
+    // Obtener los envíos almacenados en el almacenamiento local
+    let envios = JSON.parse(localStorage.getItem("envios")) || [];
+
+    // Función para eliminar los datos de la tabla y del almacenamiento local
+    function eliminarDatos() {
+        tablaResultados.innerHTML = ""; // Limpia el contenido de la tabla
+        envios = []; // Borra todos los datos en el arreglo envios
+        localStorage.removeItem("envios"); // Borra los datos almacenados en el almacenamiento local
+    }
+
+    // Recorrer los envíos y agregar filas a la tabla
     envios.forEach((envio) => {
         const fila = document.createElement("tr");
         fila.innerHTML = `
@@ -17,4 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         `;
         tablaResultados.appendChild(fila);
     });
+
+    // Agregar un evento de clic al botón "Eliminar Datos" para eliminar permanentemente los datos
+    document.getElementById("eliminarDatos").addEventListener("click", eliminarDatos);
 });
